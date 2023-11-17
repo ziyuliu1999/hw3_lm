6de49064e0684521c2bb16eac0592c4bffc174ba
ziyuliu <ziyuliu@umich.edu>
2023-11-16 21:45:01 -0500

    message

--- tests/testthat/test-hw3lm.R
+++ tests/testthat/test-hw3lm.R
@@ -2,6 +2,8 @@ test_that("linear_regression works", {
   x = matrix(rnorm(3*100), nrow = 100, ncol = 3)
   y = matrix(rnorm(1*100), nrow = 100, ncol = 1)
   res = linear_regression(x, y)
-  lm_res = lm(y ~ x)$coefficients
-  expect_equal(res$coefficients, lm_res)
+  lm_res = summary(lm(y ~ x))$coefficients
+  std_err_lm = lm_res[ , 2]
+  expect_equal(res$coefficients, lm_res[ , 1])
+  expect_equal(res$std_err, std_err_lm)
 })
