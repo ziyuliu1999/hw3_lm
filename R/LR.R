b6a13f9a63fee1f700df40b19d9472be73eb7240
ziyuliu <ziyuliu@umich.edu>
2023-11-17 19:51:53 -0500

    message

--- R/LR.R
+++ R/LR.R
@@ -13,25 +13,31 @@
 #' @export
 #'
 
-
+# Here is the linear_regression function
 linear_regression = function (X, Y) {
+  # Transform possible dataframe to matrix
   X = cbind(rep(1, nrow(X)), X)
   X = as.matrix(X)
   Y = as.matrix(Y)
+  # Here is the estimated coefficient calculate
   beta_hat = solve(t(X) %*% X) %*% t(X) %*% Y
   err_var = sum((X %*% beta_hat - Y)^2) / (nrow(X) - ncol(X))
   std_err = sqrt(err_var)
   std_err_1 = c()
+  # Here is the standard error calculate
   for (i in 1:nrow(beta_hat)) {
     std_err_1 = c(std_err_1, sqrt(solve((t(X) %*% X))[i,i] * err_var))
   }
+  # Here is the t value calculation
   t_val = beta_hat / std_err_1
   p_val = c()
+  # Here is the P-value calculation
   for (i in 1:nrow(t_val)) {
     p_val = c(p_val, 2 * pt(abs(t_val[i]), df = nrow(X) - ncol(X), lower.tail = FALSE))
   }
   beta_hat = t(beta_hat)
   beta_hat = c(beta_hat)
+  # Assign the column names to those values
   if (is.null(colnames(X))) {
     nmb_pred = ncol(X) - 1
     col_nam = 1:nmb_pred
@@ -44,6 +50,7 @@ linear_regression = function (X, Y) {
   t_val = c(t(t_val))
   names(t_val) = c("(Intercept)", col_nam)
   names(p_val) = c("(Intercept)", col_nam)
+  # Combine those data to a table
   tble = cbind(beta_hat, std_err_1, t_val, p_val)
   colnames(tble) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
   return(tble = tble)
